6970853d17d08f25c4231fd6f565c1389ea4a188
euripus <epistomoclus@gmail.com>
2019-11-27 10:35:20 +0200

    fixes

--- src/render/gl/globjwrapper.h
+++ src/render/gl/globjwrapper.h
@@ -1,256 +1,179 @@
 #ifndef GLOBJWRAPPER_H
 #define GLOBJWRAPPER_H
 
+#include <GL/glew.h>
 #include <utility>
-#include <GL/gl.h>
 
 namespace GLObjectWrappers
 {
-template<class CreateReleaseHelperType>
-class GLObjWrapper
+template<typename GeneratorType>
+class GLObjectWrapper
 {
+    GeneratorType m_gen;
+    GLuint        m_gl_obj_id{0};
+
 public:
-    GLObjWrapper(bool CreateObject, CreateReleaseHelperType CreateReleaseHelper = CreateReleaseHelperType()) :
-        m_uiHandle(0), m_CreateReleaseHelper(CreateReleaseHelper)
+    GLObjectWrapper(bool create)
     {
-        if(CreateObject)
+        if(create)
+            m_gl_obj_id = m_gen.Create();
+    }
+
+    ~GLObjectWrapper()
+    {
+        if(m_gl_obj_id != 0)
         {
-            Create();
-            if(!m_uiHandle)
-            {
-                LOG_ERROR_AND_THROW("Failed to create ", m_CreateReleaseHelper.Name, " GL object");
-            }
+            m_gen.Release(m_gl_obj_id);
+            m_gl_obj_id = 0;
         }
     }
 
-    ~GLObjWrapper() { Release(); }
+    GLObjectWrapper(const GLObjectWrapper &) = delete;
+    GLObjectWrapper & operator=(const GLObjectWrapper &) = delete;
 
-    GLObjWrapper(GLObjWrapper && Wrapper) :
-        m_uiHandle(Wrapper.m_uiHandle),
-        m_CreateReleaseHelper(std::move(Wrapper.m_CreateReleaseHelper))
+    GLObjectWrapper & operator=(GLObjectWrapper && other)
     {
-        Wrapper.m_uiHandle = 0;
-    }
-
-    GLObjWrapper & operator=(GLObjWrapper && Wrapper)
-    {
-        Release();
-        m_uiHandle            = Wrapper.m_uiHandle;
-        Wrapper.m_uiHandle    = 0;
-        m_CreateReleaseHelper = std::move(Wrapper.m_CreateReleaseHelper);
+        std::swap(m_gl_obj_id, other.m_gl_obj_id);
+        std::swap(m_gen, other.m_gen);
         return *this;
     }
 
-    void Create()
+    GLObjectWrapper(GLObjectWrapper && other)
     {
-        VERIFY(m_uiHandle == 0, "GL object is already initialized");
-        Release();
-        m_CreateReleaseHelper.Create(m_uiHandle);
-        VERIFY(m_uiHandle, "Failed to initialize GL object");
+        m_gl_obj_id = other.m_gl_obj_id;
+        m_gen       = other.m_gen;
     }
 
-    void Release()
+    operator GLuint() const { return m_gl_obj_id; }
+
+    static GLObjectWrapper Null() { return GLObjectWrapper{false}; }
+};
+
+class TexGenerator
+{
+public:
+    GLuint Create() const
     {
-        if(m_uiHandle)
-        {
-            m_CreateReleaseHelper.Release(m_uiHandle);
-            m_uiHandle = 0;
-        }
+        GLuint new_tex{0};
+        glGenTextures(1, &new_tex);
+
+        return new_tex;
     }
 
-    operator GLuint() const { return m_uiHandle; }
+    void Release(GLuint tex) { glDeleteTextures(1, &tex); }
+};
+
+class GLBufferObjGenerator
+{
+public:
+    GLuint Create() const
+    {
+        GLuint new_buff_obj{0};
+        glGenBuffers(1, &new_buff_obj);
+
+        return new_buff_obj;
+    }
+
+    void Release(GLuint buff) { glDeleteBuffers(1, &buff); }
+};
+
+class GLProgramObjGenerator
+{
+public:
+    GLuint Create() const { return glCreateProgram(); }
+    void   Release(GLuint prg) { glDeleteProgram(prg); }
+};
+
+class GLShaderObjGenerator
+{
+public:
+    GLShaderObjGenerator(GLenum ShaderType) : m_shader_type(ShaderType) {}
+    GLuint Create() const { return glCreateShader(m_shader_type); }
+    void   Release(GLuint shd) { glDeleteShader(shd); }
 
 private:
-    GLObjWrapper(const GLObjWrapper &);
-    GLObjWrapper & operator=(const GLObjWrapper &);
-
-    GLuint                  m_uiHandle;
-    CreateReleaseHelperType m_CreateReleaseHelper;
+    GLenum m_shader_type;
 };
 
-class GLBufferObjCreateReleaseHelper
+class GLPipelineObjGenerator
 {
 public:
-    explicit GLBufferObjCreateReleaseHelper(GLuint ExternalGLBufferHandle = 0) :
-        m_ExternalGLBufferHandle(ExternalGLBufferHandle)
-    {}
-
-    void Create(GLuint & BuffObj)
+    GLuint Create() const
     {
-        if(m_ExternalGLBufferHandle != 0)
-            BuffObj = m_ExternalGLBufferHandle;   // Attach to external GL buffer handle
-        else
-            glGenBuffers(1, &BuffObj);
+        GLuint new_pipeline{0};
+        glGenProgramPipelines(1, &new_pipeline);
+
+        return new_pipeline;
     }
 
-    void Release(GLuint BuffObj)
-    {
-        if(m_ExternalGLBufferHandle != 0)
-            m_ExternalGLBufferHandle = 0;   // Detach from external GL buffer. DO NOT delete the buffer
-        else
-            glDeleteBuffers(1, &BuffObj);
-    }
-    static const char * Name;
-
-private:
-    GLuint m_ExternalGLBufferHandle;
+    void Release(GLuint pipeline) { glDeleteProgramPipelines(1, &pipeline); }
 };
-typedef GLObjWrapper<GLBufferObjCreateReleaseHelper> GLBufferObj;
 
-class GLProgramObjCreateReleaseHelper
+class GLVertexArrayObjGenerator
 {
 public:
-    static void         Create(GLuint & ProgObj) { ProgObj = glCreateProgram(); }
-    static void         Release(GLuint ProgObj) { glDeleteProgram(ProgObj); }
-    static const char * Name;
-};
-typedef GLObjWrapper<GLProgramObjCreateReleaseHelper> GLProgramObj;
-
-class GLShaderObjCreateReleaseHelper
-{
-public:
-    GLShaderObjCreateReleaseHelper(GLenum ShaderType) : m_ShaderType(ShaderType) {}
-    void                Create(GLuint & ShaderObj) { ShaderObj = glCreateShader(m_ShaderType); }
-    void                Release(GLuint ShaderObj) { glDeleteShader(ShaderObj); }
-    static const char * Name;
-
-private:
-    GLenum m_ShaderType;
-};
-typedef GLObjWrapper<GLShaderObjCreateReleaseHelper> GLShaderObj;
-
-class GLPipelineObjCreateReleaseHelper
-{
-public:
-    void                Create(GLuint & Pipeline) { glGenProgramPipelines(1, &Pipeline); }
-    void                Release(GLuint Pipeline) { glDeleteProgramPipelines(1, &Pipeline); }
-    static const char * Name;
-};
-typedef GLObjWrapper<GLPipelineObjCreateReleaseHelper> GLPipelineObj;
-
-class GLVAOCreateReleaseHelper
-{
-public:
-    void                Create(GLuint & VAO) { glGenVertexArrays(1, &VAO); }
-    void                Release(GLuint VAO) { glDeleteVertexArrays(1, &VAO); }
-    static const char * Name;
-};
-typedef GLObjWrapper<GLVAOCreateReleaseHelper> GLVertexArrayObj;
-
-class GLTextureCreateReleaseHelper
-{
-public:
-    explicit GLTextureCreateReleaseHelper(GLuint ExternalGLTextureHandle = 0) :
-        m_ExternalGLTextureHandle(ExternalGLTextureHandle)
-    {}
-
-    void Create(GLuint & Tex)
+    GLuint Create() const
     {
-        if(m_ExternalGLTextureHandle != 0)
-            Tex = m_ExternalGLTextureHandle;   // Attach to the external texture
-        else
-            glGenTextures(1, &Tex);
+        GLuint new_vao{0};
+        glGenVertexArrays(1, &new_vao);
+
+        return new_vao;
     }
 
-    void Release(GLuint Tex)
-    {
-        if(m_ExternalGLTextureHandle != 0)
-            m_ExternalGLTextureHandle = 0;   // Detach from the external texture. DO NOT delete it!
-        else
-            glDeleteTextures(1, &Tex);
-    }
-
-    static const char * Name;
-
-private:
-    GLuint m_ExternalGLTextureHandle;
+    void Release(GLuint vao) { glDeleteVertexArrays(1, &vao); }
 };
-typedef GLObjWrapper<GLTextureCreateReleaseHelper> GLTextureObj;
 
-class GLSamplerCreateReleaseHelper
+class GLSamplerObjGenerator
 {
 public:
-    void                Create(GLuint & Sampler) { glGenSamplers(1, &Sampler); }
-    void                Release(GLuint Sampler) { glDeleteSamplers(1, &Sampler); }
-    static const char * Name;
-};
-typedef GLObjWrapper<GLSamplerCreateReleaseHelper> GLSamplerObj;
+    GLuint Create() const
+    {
+        GLuint new_smpl{0};
+        glGenSamplers(1, &new_smpl);
 
-class GLFBOCreateReleaseHelper
+        return new_smpl;
+    }
+
+    void Release(GLuint smpl) { glDeleteSamplers(1, &smpl); }
+};
+
+class GLFBOGenerator
 {
 public:
-    explicit GLFBOCreateReleaseHelper(GLuint ExternalFBOHandle = 0) : m_ExternalFBOHandle(ExternalFBOHandle)
-    {}
-
-    void Create(GLuint & FBO)
+    GLuint Create() const
     {
-        if(m_ExternalFBOHandle != 0)
-            FBO = m_ExternalFBOHandle;   // Attach to external FBO handle
-        else
-            glGenFramebuffers(1, &FBO);
+        GLuint fbo{0};
+        glGenFramebuffers(1, &fbo);
+
+        return fbo;
     }
 
-    void Release(GLuint FBO)
-    {
-        if(m_ExternalFBOHandle != 0)
-            m_ExternalFBOHandle = 0;   // DO NOT delete the FBO
-        else
-            glDeleteFramebuffers(1, &FBO);
-    }
-
-    static const char * Name;
-
-private:
-    GLuint m_ExternalFBOHandle;
+    void Release(GLuint fbo) { glDeleteFramebuffers(1, &fbo); }
 };
-typedef GLObjWrapper<GLFBOCreateReleaseHelper> GLFrameBufferObj;
 
-class GLRBOCreateReleaseHelper
+class GLRBOGenerator
 {
 public:
-    void                Create(GLuint & RBO) { glGenRenderbuffers(1, &RBO); }
-    void                Release(GLuint RBO) { glDeleteRenderbuffers(1, &RBO); }
-    static const char * Name;
-};
-typedef GLObjWrapper<GLRBOCreateReleaseHelper> GLRenderBufferObj;
-
-struct GLSyncObj
-{
-    GLSyncObj() {}
-    GLSyncObj(GLsync _SyncHandle) : SyncHandle(_SyncHandle) {}
-
-    GLSyncObj(const GLSyncObj &) = delete;
-    GLSyncObj & operator=(const GLSyncObj &) = delete;
-
-    GLSyncObj(GLSyncObj && rhs)
+    GLuint Create() const
     {
-        SyncHandle     = rhs.SyncHandle;
-        rhs.SyncHandle = GLsync{};
+        GLuint rbo{0};
+        glGenRenderbuffers(1, &rbo);
+
+        return rbo;
     }
 
-    GLSyncObj & operator=(GLSyncObj && rhs)
-    {
-        Release();
-        SyncHandle     = rhs.SyncHandle;
-        rhs.SyncHandle = GLsync{};
-        return *this;
-    }
-
-    void Release()
-    {
-        if(SyncHandle != GLsync{})
-            glDeleteSync(SyncHandle);
-        SyncHandle = GLsync{};
-    }
-
-    ~GLSyncObj() { Release(); }
-
-    operator GLsync() const { return SyncHandle; }
-
-private:
-    GLsync SyncHandle = {};
+    void Release(GLuint rbo) { glDeleteRenderbuffers(1, &rbo); }
 };
 
+using GLTextureObj      = GLObjectWrapper<TexGenerator>;
+using GLBufferObj       = GLObjectWrapper<GLBufferObjGenerator>;
+using GLProgramObj      = GLObjectWrapper<GLProgramObjGenerator>;
+using GLShaderObj       = GLObjectWrapper<GLShaderObjGenerator>;
+using GLPipelineObj     = GLObjectWrapper<GLPipelineObjGenerator>;
+using GLVertexArrayObj  = GLObjectWrapper<GLVertexArrayObjGenerator>;
+using GLSamplerObj      = GLObjectWrapper<GLSamplerObjGenerator>;
+using GLFrameBufferObj  = GLObjectWrapper<GLFBOGenerator>;
+using GLRenderBufferObj = GLObjectWrapper<GLRBOGenerator>;
 }   // namespace GLObjectWrappers
 
 #endif   // GLOBJWRAPPER_H
