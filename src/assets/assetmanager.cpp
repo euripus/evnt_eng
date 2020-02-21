18384b59506b910b7a271bbca7d97147cf4d2752
euripus <epistomoclus@gmail.com>
2020-02-21 13:23:52 +0200

    x

--- src/assets/assetmanager.cpp
+++ src/assets/assetmanager.cpp
@@ -1,1 +1,32 @@
 #include "assetmanager.h"
+
+namespace evnt
+{
+IAsset * AssetManager::locateAsset(std::string path)
+{
+    auto asset = m_assets.find(path);
+    return asset == m_assets.end() ? nullptr : asset->second.get();
+}
+
+IAssetReader * AssetManager::locateAssetReader(std::string ext)
+{
+    auto asset_reader = m_readers.find(ext);
+    return asset_reader == m_readers.end() ? nullptr : asset_reader->second.get();
+}
+
+void AssetManager::registerReader(AssetReaderPtr reader)
+{
+    std::string ext = reader->getExtension();
+    m_readers.insert_or_assign(ext, std::move(reader));
+}
+
+void AssetManager::addAsset(AssetPtr asset)
+{
+    m_assets.insert_or_assign(asset->getPath(), std::move(asset));
+}
+
+void AssetManager::deleteAsset(IAsset * asset)
+{
+    m_assets.erase(asset->getPath());
+}
+}   // namespace evnt
--- src/assets/assetmanager.h
+++ src/assets/assetmanager.h
@@ -1,7 +1,8 @@
 #ifndef ASSETMANAGER_H
 #define ASSETMANAGER_H
 
-#include "../fs/memory_stream.h"
+#include "../fs/file.h"
+#include <unordered_map>
 
 namespace evnt
 {
@@ -17,15 +18,61 @@ enum class AssetReadingPriority
 class IAssetReader
 {
 public:
-    virtual AssetReadingPriority     getPriority() { return AssetReadingPriority::Middle; }
-    virtual std::vector<std::string> getExtensions()                                                  = 0;
-    virtual void loadAssets(std::string ext, InputMemoryStream & stream, AssetManager & assetManager) = 0;
+    virtual AssetReadingPriority getPriority() { return AssetReadingPriority::Middle; }
+    virtual std::string          getExtension()                                                       = 0;
+    virtual void                 loadAssets(InputMemoryStream & stream, AssetManager & asset_manager) = 0;
 };
 
+class IAsset
+{
+public:
+    virtual ~IAsset() {}
+
+    virtual std::string getPath() const          = 0;
+    virtual void        setStream(InFile stream) = 0;
+    virtual void        load()                   = 0;
+    virtual void        unload()                 = 0;
+    virtual bool        isReady()                = 0;
+    virtual bool        isValid()                = 0;
+
+    virtual IAsset * createCopy(std::string new_path) = 0;
+    virtual void     exportAsset()                    = 0;
+
+    bool operator==(IAsset & asset) const { return getPath() == asset.getPath(); }
+};
+
+// Resources owner
 class AssetManager
 {
 public:
+    using AssetPtr       = std::unique_ptr<IAsset>;
+    using AssetReaderPtr = std::unique_ptr<IAssetReader>;
+
     AssetManager();
+
+    IAsset *       locateAsset(std::string path);
+    IAssetReader * locateAssetReader(std::string ext);
+
+    void registerReader(AssetReaderPtr reader);   // Pointer will be owned by the AssetManager instance
+    void addAsset(AssetPtr asset);                // Pointer will be owned by the AssetManager instance
+    void deleteAsset(IAsset * asset);
+
+    template<typename T>
+    void setDefaultAsset(std::unique_ptr<T> asset)   // Pointer will be owned by the AssetManager instance
+    {
+        m_defaultAssets.insert({typeid(T).name(), std::move(asset)});
+    }
+
+    template<typename T>
+    T & getDefaultAsset()
+    {
+        return dynamic_cast<T &>(*m_defaultAssets.at(typeid(T).name()).get());
+    }
+
+private:
+    std::unordered_map<std::string, AssetPtr>       m_assets;
+    std::unordered_map<std::string, AssetReaderPtr> m_readers;
+    std::unordered_map<std::string, AssetPtr>       m_defaultAssets;
 };
 }   // namespace evnt
 
